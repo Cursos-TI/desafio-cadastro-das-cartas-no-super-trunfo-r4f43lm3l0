3d3fe89001a2271eca3c5961806d2be17aedf536
r4f43lm3l0 <h4ck3rm3l0@gmail.com>
2025-03-26 14:08:15 +0000

    jokenpo

--- n2_nc4_3.c
+++ n2_nc4_3.c
@@ -1,12 +1,66 @@
-#include <stdio.h>;
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
 
 int main(){
 
-    int a = 10;
-   int b = 3;
-    float quociente = (float)a / b; // 'a' é convertido explicitamente para float
+    int escolhajogador, escolhacomputador;
+    srand(time(0));
 
-    printf("Quociente : %.2f\n", quociente);
+    printf("Jogo de Jokenpô\n");
+    printf("Escolha uma opção:\n");
+    printf("1. Pedra\n");
+    printf("2. Papel\n");
+    printf("3. Tesoura\n");
+    printf("Escolha: ");
+    scanf("%d", &escolhajogador);
+
+    escolhacomputador = rand() % 3 + 1;
+
+    switch (escolhajogador)
+    {
+        case 1:
+            printf("Jogador: Pedra - ");
+            break;
+        case 2:
+            printf("Jogador: Papel - ");
+            break;
+        case 3:
+            printf("Jogador: Tesoura - ");  
+            break;
+        default:    
+            printf("Opção Inválida\n");
+            break;
+    }
+    switch (escolhacomputador)
+    {
+        case 1:
+            printf("Jogador: Pedra\n");
+            break;
+        case 2:
+            printf("Jogador: Papel\n ");
+            break;
+        case 3:
+            printf("Jogador: Tesoura\\n ");  
+            break;
+    }
+            if (escolhacomputador == escolhajogador)
+            {
+                printf("### Jogo empatou! ###\n");
+            }else if ((escolhajogador ==1) && (escolhajogador ==3) ||
+                     (escolhajogador ==2) && (escolhajogador ==1)||
+                     (escolhajogador ==3) && (escolhajogador ==2))
+            {
+                printf("### Parabéns, você ganhou!\n");
+            } else {
+                printf("###Você Perdeu!###");
+            }
+
+
+
+
+
 
     return 0;
 
